ed6bd41fddde9324c8d2ced645e0afefe66860a6
LamLe <letruonglam2511@gmail.com>
2023-07-13 21:21:30 +0700

    Stash back to Second

--- test..cpp
+++ test..cpp
@@ -4,5 +4,10 @@ using namespace std;
 
 int main() {
     cout << "ahihi" << endl;
+<<<<<<< Updated upstream
     cout << " from second " << endl;
+=======
+
+    cout << "stahs ne`" << endl;
+>>>>>>> Stashed changes
 }
