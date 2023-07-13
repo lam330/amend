e0ed7ccd539b60a5dae8b50472ecaa5191838c11
LamLe <letruonglam2511@gmail.com>
2023-07-13 22:00:05 +0700

    abcdef

--- test..cpp
+++ test..cpp
@@ -4,17 +4,7 @@ using namespace std;
 
 int main() {
     cout << "ahihi" << endl;
-<<<<<<< Updated upstream
     cout << " from second " << endl;
-<<<<<<< Updated upstream
-=======
-
     cout << "stahs ne`" << endl;
->>>>>>> Stashed changes
-=======
-
-
-
     cout << "stash2 nhe'" << endl;
->>>>>>> Stashed changes
 }
